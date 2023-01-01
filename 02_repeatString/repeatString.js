0326c8003102b5165e51f1ee099f021f5ea20adc
Phil Taylor <phil.c.taylor@gmail.com>
2022-12-31 19:09:11 -0500

    completed first two exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let output = "";
+    let repetitions = 0;
+    while (repetitions < num) {
+        output = output.concat(string);
+        repetitions++;
+    }
+    return output;
+}
 
 // Do not edit below this line
 module.exports = repeatString;

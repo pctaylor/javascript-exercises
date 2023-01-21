5be5b0a1fcbfdea5eaaeaf2e270c9dcdd36ea94d
Phil Taylor <phil.c.taylor@gmail.com>
2023-01-21 16:15:43 -0500

    wrapping 08_calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,25 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
-};
+const subtract = (num1, num2) => { return (num1 - num2)};
 
-const sum = function() {
-	
-};
+const sum = function sum(arr) {
+  return arr.reduce((acc, current) => acc + current, 0);
+}
 
-const multiply = function() {
+const multiply = function multiply(arr) {
+  return arr.reduce((acc, current) => acc * current, 1);
+}
 
-};
+const power = (num1, num2) => { return num1 ** num2};
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
